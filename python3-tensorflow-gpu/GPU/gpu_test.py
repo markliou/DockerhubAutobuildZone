d0cc3d180d92f86afe0ccd51c0648ce36ba04db3
markliou <yifanliou@markliou.tw>
2024-01-21 00:51:38 +0800

    container tester

---
+++ python3-tensorflow-gpu/GPU/gpu_test.py
@@ -0,0 +1,23 @@
+import tensorflow as tf 
+import jax 
+import torch 
+
+
+def check_tf():
+    pass
+
+def check_jax():
+    pass
+
+def check_torch():
+    pass
+
+
+def main():
+    check_tf()
+    check_jax()
+    check_torch()
+
+
+if __name__ == "__main__":
+    main()
