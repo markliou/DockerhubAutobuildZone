d62ed77db2f20cff201aae72eb4d29c40e58d603
markliou <yifanliou@markliou.tw>
2024-01-21 16:25:08 +0800

    use bin instead the alias

--- dl-container/GPU/gpu_test.py
+++ dl-container/GPU/gpu_test.py
@@ -1,17 +1,20 @@
-import tensorflow as tf 
-import jax 
-import torch 
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import jax
+import torch
 
 
 def check_tf():
-    assert(len(tf.config.list_physical_devices('GPU')) > 0)
+    assert (len(tf.config.list_physical_devices('GPU')) > 0)
+
 
 def check_jax():
-    assert(jax.devices()[0].platform != 'cpu')
+    assert (jax.devices()[0].platform != 'cpu')
 
 
 def check_torch():
-    assert(torch.cuda.is_available())
+    assert (torch.cuda.is_available())
 
 
 def main():
