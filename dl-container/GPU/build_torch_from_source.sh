9334ea7137545a8c20a1ae4efe1d3b7c272c36b1
markliou <yifanliou@markliou.tw>
2024-02-05 16:11:48 +0800

    build torch audio vision from source

---
+++ dl-container/GPU/build_torch_from_source.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/bash
+
+export _GLIBCXX_USE_CXX11_ABI=1
+
+apt update -y ; apt install git cuda-toolkit-12-3
+
+# install torch
+cd /root
+git clone --recursive https://github.com/pytorch/pytorch
+cd pytorch
+# if you are updating an existing checkout
+git submodule sync
+git submodule update --init --recursive
+pip3 install cmake ninja mkl-static mkl-include
+pip3 install -r requirements.txt
+python setup.py bdist_wheel
+pip3 install `ls dist/*`
+
+# install torch-audio
+cd /root
+git clone --recursive https://github.com/pytorch/audio
+cd audio
+git submodule sync
+git submodule update --init --recursive
+pip3 install -r requirements.txt
+python setup.py bdist_wheel
+pip3 install `ls dist/*`
+
+# install torch-vision
+cd /root
+git clone --recursive https://github.com/pytorch/audio
+cd audio
+git submodule sync
+git submodule update --init --recursive
+pip3 install -r requirements.txt
+python setup.py bdist_wheel
+pip3 install `ls dist/*`
